6aca8a372cc373cc93890bfbb0badd9f2e102ae8
PardCode <pengine3d@gmail.com>
2020-03-29 00:25:21 +0100

    Fixed codecvt problem for VS 2019 C++17 - Fixed experimental::filesystem for VS 2019
    Fixed codecvt problem for VS 2019 C++17 - Fixed experimental::filesystem for VS 2019

--- Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
+++ Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
@@ -1,3 +1,13 @@
+// Copyright (c) 2019 - 2020 PardCode
+// All rights reserved.
+//
+// This file is part of CPP-3D-Game-Tutorial-Series Project, accessible from https://github.com/PardCode/CPP-3D-Game-Tutorial-Series
+// You can redistribute it and/or modify it under the terms of the GNU
+// General Public License 
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 #include "ResourceManager.h"
 #include <filesystem>
 
@@ -12,7 +22,15 @@ ResourceManager::~ResourceManager()
 
 ResourcePtr ResourceManager::createResourceFromFile(const wchar_t * file_path)
 {
+
+#if _MSC_VER >= 1900 && _MSC_VER <= 1916
 	std::wstring full_path = std::experimental::filesystem::absolute(file_path);
+#endif
+
+#if _MSC_VER >= 1920
+	std::wstring full_path = std::filesystem::absolute(file_path);
+#endif
+
 
 	auto it = m_map_resources.find(full_path);
 
