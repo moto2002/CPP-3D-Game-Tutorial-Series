41b90500ef7a240ea5d90c3cb9360c9978fd6501
PardCode <pengine3d@gmail.com>
2020-03-29 01:04:35 +0100

    fixed problem relative to filesystem in VS 2019 with C++14
    fixed problem relative to filesystem in VS 2019 with C++14

--- Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
+++ Tutorial18_GraphicsEngine_RenderingModel/ResourceManager.cpp
@@ -9,7 +9,15 @@
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 #include "ResourceManager.h"
+
+#if __cplusplus <= 201402L 
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <experimental/filesystem>
+#endif
+
+#if __cplusplus >= 201703L
 #include <filesystem>
+#endif
 
 ResourceManager::ResourceManager()
 {
@@ -23,13 +31,14 @@ ResourceManager::~ResourceManager()
 ResourcePtr ResourceManager::createResourceFromFile(const wchar_t * file_path)
 {
 
-#if _MSC_VER >= 1900 && _MSC_VER <= 1916
+#if (_MSC_VER >= 1900 && _MSC_VER <= 1916)  || ( _MSC_VER >= 1920 && __cplusplus <= 201402L) 
 	std::wstring full_path = std::experimental::filesystem::absolute(file_path);
 #endif
 
-#if _MSC_VER >= 1920
+#if _MSC_VER >= 1920 && __cplusplus > 201402L 
 	std::wstring full_path = std::filesystem::absolute(file_path);
 #endif
+
 
 
 	auto it = m_map_resources.find(full_path);
